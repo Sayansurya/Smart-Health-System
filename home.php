07ab372d0fdd13a761ad8ad994ca5a42fcee019f
DhimayiSadhukhan <81740467+DhimayiSadhukhan@users.noreply.github.com>
2021-04-01 16:10:16 +0530

    Update home.php

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
-<title>Welcome to Finance Portal</title>
+<title>Welcome to Smart Health</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="assests/css/style.css">
